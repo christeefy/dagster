90895d402667d30c4368d59e42a9422f2127d4db
Christopher Tee <christopherfytee@gmail.com>
2024-12-18 19:52:54 -0500

    🔨 Add gcp_credentials to GCSResource and GCSFileManagerResource

--- python_modules/libraries/dagster-gcp/dagster_gcp/gcs/resources.py
+++ python_modules/libraries/dagster-gcp/dagster_gcp/gcs/resources.py
@@ -1,10 +1,11 @@
-from typing import Any, Optional
+from typing import Any, Iterator, Optional
 
 from dagster import ConfigurableResource, IAttachDifferentObjectToOpContext, resource
 from dagster._core.definitions.resource_definition import dagster_maintained_resource
 from google.cloud import storage
 from pydantic import Field
 
+from dagster_gcp.bigquery.utils import setup_gcp_creds
 from dagster_gcp.gcs.file_manager import GCSFileManager
 
 
@@ -22,13 +23,23 @@ class GCSResource(ConfigurableResource, IAttachDifferentObjectToOpContext):
     """
 
     project: Optional[str] = Field(default=None, description="Project name")
+    gcp_credentials: Optional[str] = Field(
+        default=None,
+        description=(
+            "GCP authentication credentials. If provided, a temporary file will be created"
+            " with the credentials and ``GOOGLE_APPLICATION_CREDENTIALS`` will be set to the"
+            " temporary file. To avoid issues with newlines in the keys, you must base64"
+            " encode the key. You can retrieve the base64 encoded key with this shell"
+            " command: ``cat $GOOGLE_AUTH_CREDENTIALS | base64``"
+        ),
+    )
 
-    def get_client(self) -> storage.Client:
+    def get_client(self) -> Iterator[storage.Client]:
         """Creates a GCS Client.
 
         Returns: google.cloud.storage.Client
         """
-        return _gcs_client_from_config(project=self.project)
+        yield _gcs_client_from_config(project=self.project, gcp_credentials=self.gcp_credentials)
 
     def get_object_to_set_on_execution_context(self) -> Any:
         return self.get_client()
@@ -39,32 +50,46 @@ class GCSResource(ConfigurableResource, IAttachDifferentObjectToOpContext):
     config_schema=GCSResource.to_config_schema(),
     description="This resource provides a GCS client",
 )
-def gcs_resource(init_context) -> storage.Client:
-    return GCSResource.from_resource_context(init_context).get_client()
+def gcs_resource(init_context) -> Iterator[storage.Client]:
+    with GCSResource.from_resource_context(init_context).get_client() as client:
+        yield client
 
 
 class GCSFileManagerResource(ConfigurableResource, IAttachDifferentObjectToOpContext):
     """FileManager that provides abstract access to GCS."""
 
     project: Optional[str] = Field(default=None, description="Project name")
+    gcp_credentials: Optional[str] = Field(
+        default=None,
+        description=(
+            "GCP authentication credentials. If provided, a temporary file will be created"
+            " with the credentials and ``GOOGLE_APPLICATION_CREDENTIALS`` will be set to the"
+            " temporary file. To avoid issues with newlines in the keys, you must base64"
+            " encode the key. You can retrieve the base64 encoded key with this shell"
+            " command: ``cat $GOOGLE_AUTH_CREDENTIALS | base64``"
+        ),
+    )
     gcs_bucket: str = Field(description="GCS bucket to store files")
     gcs_prefix: str = Field(default="dagster", description="Prefix to add to all file paths")
 
-    def get_client(self) -> GCSFileManager:
+    def get_client(self) -> Iterator[GCSFileManager]:
         """Creates a :py:class:`~dagster_gcp.GCSFileManager` object that implements the
         :py:class:`~dagster._core.storage.file_manager.FileManager` API .
 
         Returns: GCSFileManager
         """
-        gcs_client = _gcs_client_from_config(project=self.project)
-        return GCSFileManager(
-            client=gcs_client,
-            gcs_bucket=self.gcs_bucket,
-            gcs_base_key=self.gcs_prefix,
-        )
+        with _gcs_client_from_config(
+            project=self.project, gcp_credentials=self.gcp_credentials
+        ) as gcs_client:
+            yield GCSFileManager(
+                client=gcs_client,
+                gcs_bucket=self.gcs_bucket,
+                gcs_base_key=self.gcs_prefix,
+            )
 
     def get_object_to_set_on_execution_context(self) -> Any:
-        return self.get_client()
+        with self.get_client() as client:
+            yield client
 
 
 @dagster_maintained_resource
@@ -74,10 +99,13 @@ def gcs_file_manager(context):
 
     Implements the :py:class:`~dagster._core.storage.file_manager.FileManager` API.
     """
-    return GCSFileManagerResource.from_resource_context(context).get_client()
+    with GCSFileManagerResource.from_resource_context(context).get_client() as client:
+        yield client
 
 
-def _gcs_client_from_config(project: Optional[str]) -> storage.Client:
+def _gcs_client_from_config(
+    project: Optional[str], gcp_credentials: Optional[str]
+) -> Iterator[storage.Client]:
     """Creates a GCS Client.
 
     Args:
@@ -85,4 +113,8 @@ def _gcs_client_from_config(project: Optional[str]) -> storage.Client:
 
     Returns: A GCS client.
     """
-    return storage.client.Client(project=project)
+    if gcp_credentials:
+        with setup_gcp_creds(gcp_credentials):
+            yield storage.client.Client(project=project)
+    else:
+        yield storage.client.Client(project=project)
